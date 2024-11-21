02077f9fc9e7f1ca94bf5e6877fdb20a4a66a2c3
Lakshay Malhotra <lakshay.sbp@gmail.com>
2024-11-21 02:33:56 -0500

    connected db.ts

---
+++ dist/middleware.js
@@ -0,0 +1,7 @@
+"use strict";
+// import {Model, Schema} from 'mongoose';
+// const UserSchema = new Schema({
+//     username: {type: String, unique: true},
+//     password: String,
+// })
+// export const userModel = new Model( UserSchema, "user")
--- src/index.ts
+++ src/index.ts
@@ -40,3 +40,5 @@ app.post('api/v1/brain/share', (req, res) => {
 
 app.get('api/v1/brain/:shareLink', (req, res) => {
 });
+
+app.listen (3000);
